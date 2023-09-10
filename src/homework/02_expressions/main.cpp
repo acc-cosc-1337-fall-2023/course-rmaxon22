653a2d8b0b7c3add22faf66ab3d9f31f817bb44b
Ruby Nguyen <143145209+rmaxon22@users.noreply.github.com>
2023-09-10 03:11:52 +0000

    hw2

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,36 @@
 ﻿//write include statements
+TEST_CASE("Test Get Sales Tax") {
+	REQUIRE(get_sales_tax_amount(10.0) == 0.675);
+	REQUIRE(get_sales_tax_amount(20.0) == 1.35);
+}
 
+TEST_CASE("Test Get Tip Amount") {
+	REQUIRE(get_tip_amount(20.0, 15.0) == 3.0);
+	REQUIRE(get_tip_amount(20.0, 20.0) == 4.0);
+}
 
 //write namespace using statement for cout
 
 int main()
 {
-	
+	double meal_amount, tip_rate, tip_amount, tax_amount, total;
+
+	std::cout << "Enter meal amount: ";
+	std::cin >> meal_amount;
+
+	tax_amount=get_sales_tax_amount(meal_amount);
+
+	std::cout << "Enter tip rate (%): ";
+	std::cin >> tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = meal_amount + tax_amount + tip_amount;
+
+	std::cout << "Meal Amount:		" << meal_amount << std::end1;
+	std:cout << "Sales Tax:			" << tax_amount << std::end1;
+	std::cout << "Tip Amount:		" << tip_amount << std::end1;
+	std::cout << "Total:			" << total << std::end1;
 
 	return 0;
 }
